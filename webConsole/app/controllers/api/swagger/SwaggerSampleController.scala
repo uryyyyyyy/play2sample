32ffef5652f8553ffb7522c7df361e142a86dfea
uryyyyyyy <koki305@gmail.com>
2016-08-10 08:23:08 +0900

    play2-swagger

--- webConsole/app/controllers/api/swagger/SwaggerSampleController.scala
+++ webConsole/app/controllers/api/swagger/SwaggerSampleController.scala
@@ -3,18 +3,22 @@ package controllers.api.swagger
 import javax.inject.{Inject, Singleton}
 
 import akka.actor.ActorSystem
-import io.swagger.annotations.{ApiParam, ApiResponse, ApiResponses}
-import play.api.libs.ws.WSClient
+import io.swagger.annotations._
 import play.api.mvc.{Action, Controller}
 
 import scala.concurrent.ExecutionContext
-import scala.concurrent.duration._
 
 @Singleton
+@Api(value = "api sample 1")
 class SwaggerSampleController @Inject() (actorSystem: ActorSystem) extends Controller {
 
   implicit val myExecutionContext: ExecutionContext = actorSystem.dispatcher
 
+  @ApiOperation(
+    httpMethod = "GET",
+    nickname = "hello",
+    value = ""
+  )
   @ApiResponses(Array(
     new ApiResponse(code = 400, message = "Invalid ID supplied"),
     new ApiResponse(code = 404, message = "Pet not found")))
