099c3426aed20802cbd5a4a2534ab763d092eb7b
ko.shibata <ko.shibata@opt.ne.jp>
2016-08-15 17:41:38 +0900

    shell修正

--- webConsole/deployFront.sh
+++ webConsole/deployFront.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+rm -r ./public/
 cp -R ./front/public/ ./public/
--- webConsole/front/moveToPublic.sh
+++ webConsole/front/moveToPublic.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+# ライブラリを配置する
+rm -r ./public/libs
 mkdir -p ./public/libs/
 cp ./node_modules/jquery/dist/jquery.min.js ./public/libs/
